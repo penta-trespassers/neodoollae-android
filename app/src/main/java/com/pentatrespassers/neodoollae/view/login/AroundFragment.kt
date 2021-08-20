f08a79845e67f67f630a48f0f2438d27f29aff80
2019040782 <recsater@hanyang.ac.kr>
2021-08-21 03:23:00 +0900

    edit

---
+++ app/src/main/java/com/pentatrespassers/neodoollae/view/login/AroundFragment.kt
@@ -0,0 +1,62 @@
+package com.pentatrespassers.neodoollae.view.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.UiThread
+import androidx.fragment.app.Fragment
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+import com.pentatrespassers.neodoollae.R
+import com.pentatrespassers.neodoollae.databinding.FragmentAroundBinding
+
+
+class AroundFragment private constructor() : Fragment(), OnMapReadyCallback {
+
+    private lateinit var bind: FragmentAroundBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        bind = FragmentAroundBinding.inflate(inflater, container, false)
+        with(bind) {
+            val fm = childFragmentManager
+            val mapFragment = fm.findFragmentById(R.id.naverMap) as MapFragment?
+                ?: MapFragment.newInstance().also {
+                    fm.beginTransaction().add(R.id.naverMap, it).commit()
+                }
+            mapFragment.getMapAsync(this@AroundFragment)
+            return root
+        }
+
+    }
+
+    private var previousPanelHeight = 0
+
+    @UiThread
+    override fun onMapReady(naverMap: NaverMap) {
+        with (bind) {
+            naverMap.setOnMapClickListener { pointF, latLng ->
+                if (mapSearchViewAround.visibility == View.VISIBLE) {
+                    mapSearchViewAround.visibility = View.GONE
+                    previousPanelHeight = slidingUpPanel.panelHeight
+                    slidingUpPanel.panelHeight = 0
+                } else {
+                    mapSearchViewAround.visibility = View.VISIBLE
+                    slidingUpPanel.panelHeight = previousPanelHeight
+                }
+            }
+        }
+
+    }
+
+
+    companion object {
+        fun newInstance() = AroundFragment()
+    }
+}
+
