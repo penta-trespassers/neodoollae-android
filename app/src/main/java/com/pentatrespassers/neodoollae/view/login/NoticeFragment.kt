efd9fe6ea255ccf4bfc7452f9ea3d3a1d1a503ea
recsater <recsater@naver.com>
2021-08-24 13:41:53 +0900

    알림 어댑터 및 메인 액티비티 프래그먼트 이동 최적화 #27
    * 1. 지저분한 어댑터 및 프래그먼트 코드 수정
    
    * 1. 지저분한 어댑터 및 프래그먼트 코드 수정
    
    * 1. requireContext 로 수정
    
    * spinner 변수 삭제하고 bind로 사용
    
    * 1. 기존 NoticeCellRecyclerViewAdapter 삭제
    
    * Merge remote-tracking branch 'origin/develop' into improve

--- app/src/main/java/com/pentatrespassers/neodoollae/view/login/NoticeFragment.kt
+++ app/src/main/java/com/pentatrespassers/neodoollae/view/login/NoticeFragment.kt
@@ -7,22 +7,23 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.pentatrespassers.neodoollae.R
 import com.pentatrespassers.neodoollae.databinding.FragmentNoticeBinding
+import com.pentatrespassers.neodoollae.dto.Reservation
 import com.pentatrespassers.neodoollae.view.login.notice.NoticeCellRecyclerViewAdapter
-//import com.pentatrespassers.neodoollae.view.login.notice.LinearLayoutManagerWithSmoothScroller
 
 class NoticeFragment private constructor() : Fragment() {
 
     private lateinit var bind: FragmentNoticeBinding
 
-    //variables for expandable cell
-    private lateinit var delete: Button
-    private lateinit var deleteText: TextView // Text 터치시에도 delete 기능해야함
-    private lateinit var spinner: Spinner
-    //dummy dataArrayList  //private var dataList = ArrayList<Custom Class>()
-    private var dataList = arrayListOf("예약 수락", "예약 거절", "예약 대기", "예약 신청", "예약 대기")
+    //dummy dataArrayList  //private var dataList = ArrayList<Reservation>()
+    private var reservationList = arrayListOf(
+        Reservation(nickname = "윤건우", status = Reservation.STATUS_WAITING),
+        Reservation(nickname = "이서진", status = Reservation.STATUS_DECLINED),
+        Reservation(nickname = "현수빈", status = Reservation.STATUS_ACCEPTED),
+        Reservation(nickname = "김성준", status = Reservation.STATUS_WAITING),
+        Reservation(nickname = "황진하", status = Reservation.STATUS_ACCEPTED)
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,9 +31,10 @@ class NoticeFragment private constructor() : Fragment() {
     ): View {
         bind = FragmentNoticeBinding.inflate(inflater, container, false)
         with(bind) {
+            recyclerNotice.setHasFixedSize(true)
+            recyclerNotice.adapter = NoticeCellRecyclerViewAdapter(requireContext(), reservationList)
 
             //implement spinner
-            spinner = filterSpinnerNotice
             ArrayAdapter.createFromResource(
                 requireContext(),
                 R.array.spinnerArrayNotice,
@@ -41,10 +43,10 @@ class NoticeFragment private constructor() : Fragment() {
                 // Specify the layout to use when the list of choices appears
                 adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
                 // Apply the adapter to the spinner
-                spinner.adapter = adapter
+                filterSpinnerNotice.adapter = adapter
             }
 
-            spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            filterSpinnerNotice.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
                 override fun onNothingSelected(p0: AdapterView<*>?) { }
                 override fun onItemSelected(p0: AdapterView<*>?, p1: View?, position: Int, p3: Long) {
                     when(position) {
@@ -59,26 +61,6 @@ class NoticeFragment private constructor() : Fragment() {
                         }
                     }
                 }
-            }
-
-            //implement expandable cell
-            recyclerNotice.apply{
-                /*
-                // 선택한 Cell이 RecyclerView의 TOP으로 가게 하는 Custom LinearLayoutManager 사용
-                var linearLayoutManager = LinearLayoutManagerWithSmoothScroller(
-                    container?.getContext(),
-                    LinearLayoutManager.VERTICAL,
-                    false
-                )
-                */
-
-                // 안드로이드 기본 LinearLayoutManager
-                val linearLayoutManager = LinearLayoutManager(container?.getContext())
-                setHasFixedSize(true)
-                this.layoutManager = linearLayoutManager
-                val childControlRecyclerViewAdapter = NoticeCellRecyclerViewAdapter(this)
-                childControlRecyclerViewAdapter.addItem(dataList)
-                adapter = childControlRecyclerViewAdapter
             }
             return root
         }
