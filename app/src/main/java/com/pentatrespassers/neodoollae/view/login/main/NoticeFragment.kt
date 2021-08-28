4d47b2e40b05c4d3445f7b42e701d81d502f3fb8
Inticoy <inticoy0406@gmail.com>
2021-08-29 00:36:41 +0900

    1. NoticeCell -> Notification 2. 수락, 취소, 대기 아이콘 이름에 _black 제거 3. cell_notification.xml 예약, 리뷰 알림 레이아웃 4. dummy 예약, 리뷰 알림 리스트 5. Notification data class 새로 만듦 6. bg_border.xml radius 5dp로 변경

--- app/src/main/java/com/pentatrespassers/neodoollae/view/login/main/NoticeFragment.kt
+++ app/src/main/java/com/pentatrespassers/neodoollae/view/login/main/NoticeFragment.kt
@@ -8,22 +8,23 @@ import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
 import com.pentatrespassers.neodoollae.R
 import com.pentatrespassers.neodoollae.databinding.FragmentNoticeBinding
-import com.pentatrespassers.neodoollae.dto.Reservation
-import com.pentatrespassers.neodoollae.view.login.main.notice.NoticeCellRecyclerViewAdapter
+import com.pentatrespassers.neodoollae.dto.Notification
+import com.pentatrespassers.neodoollae.view.login.main.notice.NotificationRecyclerViewAdapter
 
 class NoticeFragment private constructor() : Fragment() {
 
     private lateinit var bind: FragmentNoticeBinding
 
-    //dummy dataArrayList  //private var dataList = ArrayList<Reservation>()
-    private var reservationList = arrayListOf(
-        Reservation(nickname = "윤건우", status = Reservation.STATUS_WAITING),
-        Reservation(nickname = "이서진", status = Reservation.STATUS_DECLINED),
-        Reservation(nickname = "현수빈", status = Reservation.STATUS_ACCEPTED),
-        Reservation(nickname = "김성준", status = Reservation.STATUS_WAITING),
-        Reservation(nickname = "황진하", status = Reservation.STATUS_ACCEPTED)
+    private var notificationList = arrayListOf(
+        Notification(nickname = "윤건우", time = "2021.08.28", status = Notification.STATUS_RESERVE_WAITING),
+        Notification(nickname = "이서진", time = "2021.08.28", status = Notification.STATUS_RESERVE_ACCEPTED),
+        Notification(nickname = "현수빈", time = "2021.08.28", status = Notification.STATUS_RESERVE_DECLINED),
+        Notification(nickname = "김성준", time = "2021.08.28", status = Notification.STATUS_REVIEW_HOST),
+        Notification(nickname = "황진하", time = "2021.08.28", status = Notification.STATUS_REVIEW_GUEST),
+        Notification(nickname = "에오스", time = "2021.08.28", status = Notification.STATUS_REVIEW_ROOM)
     )
 
     override fun onCreateView(
@@ -33,7 +34,8 @@ class NoticeFragment private constructor() : Fragment() {
         bind = FragmentNoticeBinding.inflate(inflater, container, false)
         with(bind) {
             recyclerNotice.setHasFixedSize(true)
-            recyclerNotice.adapter = NoticeCellRecyclerViewAdapter(requireContext(), reservationList)
+            recyclerNotice.adapter = NotificationRecyclerViewAdapter(requireContext(), notificationList)
+            recyclerNotice.addItemDecoration(DividerItemDecoration(requireContext(), 1))
 
             //implement spinner
             ArrayAdapter.createFromResource(
---
+++ app/src/main/java/com/pentatrespassers/neodoollae/view/login/main/notice/NotificationRecyclerViewAdapter.kt
@@ -0,0 +1,112 @@
+package com.pentatrespassers.neodoollae.view.login.main.notice
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.pentatrespassers.neodoollae.R
+import com.pentatrespassers.neodoollae.databinding.CellNotificationBinding
+import com.pentatrespassers.neodoollae.dto.Notification
+
+class NotificationRecyclerViewAdapter(
+    private val context: Context, private val notificationList: ArrayList<Notification>
+) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    inner class CellNoticeHolder(
+        private val bind: CellNotificationBinding
+    ) :
+        RecyclerView.ViewHolder(bind.root) {
+
+        fun binding(notification: Notification) {
+            with(bind) {
+                when (notification.status) {
+                    Notification.STATUS_RESERVE_WAITING -> {
+                        notificationTextNotice.text = notification.nickname + context.getString(R.string.reservation_waiting)
+                        notificationTimeTextNotice.text = notification.time
+                        notificationImageNotice.setImageResource(R.drawable.ic_watch_later_24dp)
+                    }
+                    Notification.STATUS_RESERVE_ACCEPTED -> {
+                        notificationTextNotice.text = notification.nickname + context.getString(R.string.reservation_accepted)
+                        notificationTimeTextNotice.text = notification.time
+                        notificationImageNotice.setImageResource(R.drawable.ic_check_circle_24dp)
+                    }
+                    Notification.STATUS_RESERVE_DECLINED -> {
+                        notificationTextNotice.text = notification.nickname + context.getString(R.string.reservation_declined)
+                        notificationTimeTextNotice.text = notification.time
+                        notificationImageNotice.setImageResource(R.drawable.ic_cancel_24dp)
+                    }
+                    Notification.STATUS_REVIEW_HOST -> {
+                        notificationTextNotice.text = notification.nickname + context.getString(R.string.review_host)
+                        notificationTimeTextNotice.text = notification.time
+                        notificationImageNotice.setImageResource(R.drawable.ic_edit_24dp)
+                    }
+                    Notification.STATUS_REVIEW_GUEST -> {
+                        notificationTextNotice.text = notification.nickname + context.getString(R.string.review_guest)
+                        notificationTimeTextNotice.text = notification.time
+                        notificationImageNotice.setImageResource(R.drawable.ic_edit_24dp)
+                    }
+                    Notification.STATUS_REVIEW_ROOM -> {
+                        notificationTextNotice.text = notification.nickname + context.getString(R.string.review_room)
+                        notificationTimeTextNotice.text = notification.time
+                        notificationImageNotice.setImageResource(R.drawable.ic_edit_24dp)
+                    }
+                }
+
+                messageToGuestTextNotice
+
+                constraintLayoutNotice.setOnClickListener {
+                    if (notification.status == 0) {
+                        if (reserveExpandedGroupNotice.visibility == View.VISIBLE) {
+                            reserveExpandedGroupNotice.visibility = View.GONE
+                            reserveUndecidedGroupNotice.visibility = View.GONE
+                        arrowImageNotice.setImageResource(R.drawable.ic_baseline_keyboard_arrow_up_24)
+                        } else {
+                            reserveExpandedGroupNotice.visibility = View.VISIBLE
+                            reserveUndecidedGroupNotice.visibility = View.VISIBLE
+                            arrowImageNotice.setImageResource(R.drawable.ic_baseline_keyboard_arrow_down_24)
+                        }
+                    } else if (notification.status < 3){
+                        if (reserveExpandedGroupNotice.visibility == View.VISIBLE) {
+                            reserveExpandedGroupNotice.visibility = View.GONE
+                            reserveDecidedGroupNotice.visibility = View.GONE
+                            arrowImageNotice.setImageResource(R.drawable.ic_baseline_keyboard_arrow_up_24)
+                        } else {
+                            reserveExpandedGroupNotice.visibility = View.VISIBLE
+                            reserveDecidedGroupNotice.visibility = View.VISIBLE
+                            arrowImageNotice.setImageResource(R.drawable.ic_baseline_keyboard_arrow_down_24)
+                        }
+                    } else {
+                        if (reviewExpandedGroupNotice.visibility == View.VISIBLE) {
+                            reviewExpandedGroupNotice.visibility = View.GONE
+                            arrowImageNotice.setImageResource(R.drawable.ic_baseline_keyboard_arrow_up_24)
+                        } else {
+                            reviewExpandedGroupNotice.visibility = View.VISIBLE
+                            arrowImageNotice.setImageResource(R.drawable.ic_baseline_keyboard_arrow_down_24)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val layoutInflater = LayoutInflater.from(context)
+        return CellNoticeHolder(CellNotificationBinding.inflate(layoutInflater, parent, false))
+    }
+
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val reservation = notificationList[position]
+        (holder as CellNoticeHolder).binding(reservation)
+    }
+
+    override fun getItemCount(): Int {
+        return notificationList.size
+    }
+
+
+}
