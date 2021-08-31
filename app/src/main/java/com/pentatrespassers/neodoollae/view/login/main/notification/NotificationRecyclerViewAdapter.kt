65c668dc057f1ddfc0b7ed23406fabecf84232ef
Geonwoo Yun <55380241+inticoy@users.noreply.github.com>
2021-08-31 23:27:18 +0900

    앱 테마 변경, notification cell 레이아웃 #45
    * add trespass colors.xml
    
    * change theme color
    
    * delete overlapped constraint layout in notice cell
    
    * 1. NoticeCell -> Notification
    
    * 사소한 string.xml 변경 (메시지 to 주인 변경)
    
    * setNotification 함수 생성
    
    * 알림 spinner option 변경
    
    * cell_notification 수정
    
    * 1. notice -> notification
    
    * Merge remote-tracking branch 'origin/develop' into feature/41_apply_t…
    
    * 1. add missing string

---
+++ app/src/main/java/com/pentatrespassers/neodoollae/view/login/main/notification/NotificationRecyclerViewAdapter.kt
@@ -0,0 +1,117 @@
+package com.pentatrespassers.neodoollae.view.login.main.notification
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.pentatrespassers.neodoollae.R
+import com.pentatrespassers.neodoollae.databinding.CellNotificationBinding
+import com.pentatrespassers.neodoollae.dto.Notification
+
+class NotificationRecyclerViewAdapter(
+    private val context: Context, private val notificationList: ArrayList<Notification>
+) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    inner class CellNoticeHolder(
+        private val bind: CellNotificationBinding
+    ) :
+        RecyclerView.ViewHolder(bind.root) {
+
+        fun binding(notification: Notification) {
+            with(bind) {
+                setNotification(notification)
+
+                constraintLayoutNotice.setOnClickListener {
+                    if (notification.status == 0) {
+                        if (reserveExpandedGroupNotice.visibility == View.VISIBLE) {
+                            reserveExpandedGroupNotice.visibility = View.GONE
+                            reserveUndecidedGroupNotice.visibility = View.GONE
+                            arrowImageNotice.setImageResource(R.drawable.ic_baseline_keyboard_arrow_down_24)
+                        } else {
+                            reserveExpandedGroupNotice.visibility = View.VISIBLE
+                            reserveUndecidedGroupNotice.visibility = View.VISIBLE
+                            arrowImageNotice.setImageResource(R.drawable.ic_baseline_keyboard_arrow_up_24)
+                        }
+                    } else if (notification.status < 3){
+                        if (reserveExpandedGroupNotice.visibility == View.VISIBLE) {
+                            reserveExpandedGroupNotice.visibility = View.GONE
+                            View.GONE.also { reserveDecidedGroupNotice.visibility = it }
+                            arrowImageNotice.setImageResource(R.drawable.ic_baseline_keyboard_arrow_down_24)
+                        } else {
+                            reserveExpandedGroupNotice.visibility = View.VISIBLE
+                            reserveDecidedGroupNotice.visibility = View.VISIBLE
+                            arrowImageNotice.setImageResource(R.drawable.ic_baseline_keyboard_arrow_up_24)
+                        }
+                    } else {
+                        if (reviewExpandedGroupNotice.visibility == View.VISIBLE) {
+                            reviewExpandedGroupNotice.visibility = View.GONE
+                            arrowImageNotice.setImageResource(R.drawable.ic_baseline_keyboard_arrow_down_24)
+                        } else {
+                            reviewExpandedGroupNotice.visibility = View.VISIBLE
+                            arrowImageNotice.setImageResource(R.drawable.ic_baseline_keyboard_arrow_up_24)
+                        }
+                    }
+                }
+            }
+        }
+
+
+        private fun setNotification(notification: Notification) {
+            with(bind) {
+                notificationTimeTextNotice.text = notification.time
+
+                when (notification.status) {
+                    Notification.STATUS_RESERVE_WAITING -> {
+                        notificationTextNotice.text =
+                            notification.nickname + context.getString(R.string.reservation_waiting)
+                        notificationImageNotice.setImageResource(R.drawable.ic_watch_later_24dp)
+                    }
+                    Notification.STATUS_RESERVE_ACCEPTED -> {
+                        notificationTextNotice.text =
+                            notification.nickname + context.getString(R.string.reservation_accepted)
+                        notificationImageNotice.setImageResource(R.drawable.ic_check_circle_24dp)
+                    }
+                    Notification.STATUS_RESERVE_DECLINED -> {
+                        notificationTextNotice.text =
+                            notification.nickname + context.getString(R.string.reservation_declined)
+                        notificationImageNotice.setImageResource(R.drawable.ic_cancel_24dp)
+                    }
+                    Notification.STATUS_REVIEW_HOST -> {
+                        notificationTextNotice.text =
+                            notification.nickname + context.getString(R.string.review_host)
+                        notificationImageNotice.setImageResource(R.drawable.ic_edit_24dp)
+                    }
+                    Notification.STATUS_REVIEW_GUEST -> {
+                        notificationTextNotice.text =
+                            notification.nickname + context.getString(R.string.review_guest)
+                        notificationImageNotice.setImageResource(R.drawable.ic_edit_24dp)
+                    }
+                    Notification.STATUS_REVIEW_ROOM -> {
+                        notificationTextNotice.text =
+                            notification.nickname + context.getString(R.string.review_room)
+                        notificationImageNotice.setImageResource(R.drawable.ic_edit_24dp)
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val layoutInflater = LayoutInflater.from(context)
+        return CellNoticeHolder(CellNotificationBinding.inflate(layoutInflater, parent, false))
+    }
+
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val reservation = notificationList[position]
+        (holder as CellNoticeHolder).binding(reservation)
+    }
+
+    override fun getItemCount(): Int {
+        return notificationList.size
+    }
+
+
+}
