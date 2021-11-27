f242e225c4f488942152aed32ca2a9ee188fb6ec
2019040782 <recsater@hanyang.ac.kr>
2021-11-28 02:01:07 +0900

    1. 커스텀 뷰 구현 초기

---
+++ app/src/main/java/com/pentatrespassers/neodoollae/common/view/BadgeImageView.kt
@@ -0,0 +1,28 @@
+package com.pentatrespassers.neodoollae.common.view
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+
+
+
+
+class BadgeImageView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : AppCompatImageView(context, attrs, defStyleAttr) {
+
+
+    val paint = Paint()
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        canvas?.apply {
+
+            paint.color = Color.RED
+
+            canvas.drawCircle(0f, 0f, 150f, paint)
+        }
+    }
+}
