988e01870b2f1abf7d8411a894a691b89e7eae16
sunny5875 <sunny5875@hanyang.ac.kr>
2022-01-16 14:52:52 +0900

    배지 눌렀을 때 정보창 띄우기 구현

--- app/src/main/java/com/pentatrespassers/neodoollae/view/login/main/AroundFragment.kt
+++ app/src/main/java/com/pentatrespassers/neodoollae/view/login/main/AroundFragment.kt
@@ -12,13 +12,19 @@ import com.naver.maps.map.LocationTrackingMode
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.overlay.InfoWindow
 import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.overlay.Overlay
 import com.naver.maps.map.util.FusedLocationSource
 import com.pentatrespassers.neodoollae.R
 import com.pentatrespassers.neodoollae.common.adapter.RoomCardAdapter
 import com.pentatrespassers.neodoollae.databinding.FragmentAroundBinding
 import com.pentatrespassers.neodoollae.dto.Room
 import com.pentatrespassers.neodoollae.view.login.main.around.MapListRecyclerViewAdapter
+import android.widget.Toast
+
+
+
 
 
 class AroundFragment private constructor() : Fragment(), OnMapReadyCallback {
@@ -38,6 +44,8 @@ class AroundFragment private constructor() : Fragment(), OnMapReadyCallback {
 
     private var mLocationSource: FusedLocationSource? = null
     private var mNaverMap: NaverMap? = null
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -85,6 +93,7 @@ class AroundFragment private constructor() : Fragment(), OnMapReadyCallback {
                     mapSearchViewAround.visibility = View.VISIBLE
                     slidingUpPanel.panelHeight = previousPanelHeight
                 }
+
             }
 
             naverMap.uiSettings.isLocationButtonEnabled = false
@@ -99,14 +108,47 @@ class AroundFragment private constructor() : Fragment(), OnMapReadyCallback {
             val mapList = makeDummyData()
 
             var markerList = mutableListOf<Marker>()
-            mapList.forEach {
-                val marker = Marker()
-                marker.position = LatLng(it.latitude, it.longitude)
-                //이미지 설정
-                // marker.setIcon(OverlayImage.fromResource(R.drawable.ic_done_24dp));
-                marker.map = naverMap
-                markerList.add(marker)
+            context?.let { it1 ->
+                mapList.forEach {
+                    val marker = Marker()
+                    marker.position = LatLng(it.latitude, it.longitude)
+                    //이미지 설정
+                    // marker.setIcon(OverlayImage.fromResource(R.drawable.ic_done_24dp));
+                    marker.map = naverMap
+                    markerList.add(marker)
+                    val infoWindow = InfoWindow()
+
+                    infoWindow.adapter = object : InfoWindow.DefaultTextAdapter(it1) {
+                        override fun getText(infoWindow: InfoWindow): CharSequence {
+                            return it.roomName
+                        }
+                    }
+                    infoWindow.alpha = 0.7f
+                    infoWindow.open(marker)
+
+                    // 마커를 클릭하면:
+                    val listener = Overlay.OnClickListener { overlay ->
+                        val marker = overlay as Marker
+
+                        if (marker.infoWindow == null) {
+                            // 현재 마커에 정보 창이 열려있지 않을 경우 엶
+                            infoWindow.open(marker)
+                        } else {
+                            // 이미 현재 마커에 정보 창이 열려있을 경우 닫음
+                            infoWindow.close()
+                        }
+                        //sliding layout 내리기
+                        mapSearchViewAround.visibility = View.GONE
+                        previousPanelHeight = slidingUpPanel.panelHeight
+                        slidingUpPanel.panelHeight = 0
+
+
+                        true
+                    }
+                    marker.onClickListener = listener
+                }
             }
+
 
 
             // NaverMap 객체 받아서 NaverMap 객체에 위치 소스 지정
@@ -118,6 +160,8 @@ class AroundFragment private constructor() : Fragment(), OnMapReadyCallback {
 
             // 권한확인. 결과는 onRequestPermissionsResult 콜백 매서드 호출
             //  ActivityCompat.requestPermissions(this@AroundFragment, PERMISSIONS, PERMISSION_REQUEST_CODE)
+
+
         }
 
     }
@@ -164,6 +208,25 @@ class AroundFragment private constructor() : Fragment(), OnMapReadyCallback {
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
+
+//    override fun onClick(p0: Overlay): Boolean {
+//        if (overlay is Marker) {
+//            val marker = overlay as Marker
+//            if (marker.infoWindow != null) {
+//                mInfoWindow.close()
+//                Toast.makeText(this.getApplicationContext(), "InfoWindow Close.", Toast.LENGTH_LONG)
+//                    .show()
+//            } else {
+//                mInfoWindow.open(marker)
+//                Toast.makeText(this.getApplicationContext(), "InfoWindow Open.", Toast.LENGTH_LONG)
+//                    .show()
+//            }
+//            return true
+//        }
+//        else{}
+//
+//        return false
+//    }
 
 
 }
