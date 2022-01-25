943566c51d55004393d8b735710b1a9aec6e6162
inticoy <inticoy0406@gmail.com>
2022-01-25 16:48:51 +0900

    1. 계정관리 cell_one_line_setting.xml 적용 2. 계정관리 cell ripple 효과 적용 3. 마이페이지 내 버튼들 textstyle bold -> normal

--- app/src/main/java/com/pentatrespassers/neodoollae/view/login/main/mypage/settings/ManageAccountFragment.kt
+++ app/src/main/java/com/pentatrespassers/neodoollae/view/login/main/mypage/settings/ManageAccountFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.pentatrespassers.neodoollae.R
 import com.pentatrespassers.neodoollae.databinding.FragmentManageAccountBinding
 
 class ManageAccountFragment private constructor() : Fragment() {
@@ -17,6 +18,24 @@ class ManageAccountFragment private constructor() : Fragment() {
     ): View {
         bind = FragmentManageAccountBinding.inflate(inflater, container, false)
         with(bind) {
+            with(logoutCell){
+                oneLineSettingImage.setImageResource(R.drawable.ic_logout_black_24dp)
+                oneLineSettingText.setText(R.string.logout)
+            }
+            with(withdrawCell){
+                oneLineSettingImage.setImageResource(R.drawable.ic_cancel_24dp)
+                oneLineSettingText.setText(R.string.withdraw)
+            }
+
+            logoutConstraint.setOnClickListener{
+
+            }
+
+            withdrawConstraint.setOnClickListener{
+
+            }
+
+
             return root
         }
     }
