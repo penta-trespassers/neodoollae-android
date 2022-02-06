86dbf1f47bb5861352d4305a00d52d190c1ac364
SEOJIN-LEE <55384156+SEOJIN-LEE@users.noreply.github.com>
2022-02-06 22:18:36 +0900

    일정 전체보기

--- app/src/main/java/com/pentatrespassers/neodoollae/view/login/main/home/EntireScheduleActivity.kt
+++ app/src/main/java/com/pentatrespassers/neodoollae/view/login/main/home/EntireScheduleActivity.kt
@@ -1,8 +1,17 @@
 package com.pentatrespassers.neodoollae.view.login.main.home
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
 import android.os.Bundle
+import android.widget.CalendarView
+import android.widget.CalendarView.OnDateChangeListener
+import androidx.appcompat.app.AppCompatActivity
+import com.pentatrespassers.neodoollae.common.adapter.RoomCardAdapter
 import com.pentatrespassers.neodoollae.databinding.ActivityEntireScheduleBinding
+import com.pentatrespassers.neodoollae.lib.Util.gone
+import com.pentatrespassers.neodoollae.lib.Util.show
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class EntireScheduleActivity : AppCompatActivity() {
 
@@ -10,10 +19,38 @@ class EntireScheduleActivity : AppCompatActivity() {
         ActivityEntireScheduleBinding.inflate(layoutInflater)
     }
 
+
+    private val favoriteRoomAdapter by lazy {
+        RoomCardAdapter(this, arrayListOf(), arrayListOf({
+            if (it.itemCount > 0) {
+                bind.bookableCardSchedule.show()
+            } else {
+                bind.notBookableTextSchedule.show()
+            }
+        }))
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         with(bind) {
             setContentView(root)
+
+            calendarView.setOnDateChangeListener(OnDateChangeListener { view, year, month, dayOfMonth ->
+                val month = month + 1
+                val calendar = Calendar.getInstance()
+                calendar.set(year, month, dayOfMonth)
+                val date = calendar.time
+                val simpledateformat = SimpleDateFormat("EEEE", Locale.getDefault())
+                val dayName: String = simpledateformat.format(date)
+
+                dateTextSchedule.text = "$year.$month.$dayOfMonth.$dayName"
+
+                backButtonSchedule.setOnClickListener {
+                    finish()
+                }
+            })
+
         }
     }
 }
