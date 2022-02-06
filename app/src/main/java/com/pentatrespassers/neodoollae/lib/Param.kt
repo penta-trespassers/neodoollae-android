768b3a2032f1cf0018239cdc6e664ca4b6265794
inticoy <inticoy0406@gmail.com>
2022-02-06 21:39:02 +0900

    ip주소 변경

--- app/src/main/java/com/pentatrespassers/neodoollae/lib/Param.kt
+++ app/src/main/java/com/pentatrespassers/neodoollae/lib/Param.kt
@@ -1,6 +1,6 @@
 package com.pentatrespassers.neodoollae.lib
 
 object Param {
-    const val DATABASE_URL = "http://13.125.207.10"
+    const val DATABASE_URL = "http://54.180.8.81"
     const val KAKAO_SDK_APP_KEY = "adb53e0f4981741cc20cfd3c0d6d3dfa"
 }
