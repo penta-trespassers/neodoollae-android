0867022a77b2985a144b8aacb72cb51878f86170
recsater <recsater@naver.com>
2022-02-13 00:40:48 +0900

    1. 권한 수정

--- app/src/main/java/com/pentatrespassers/neodoollae/view/LoginActivity.kt
+++ app/src/main/java/com/pentatrespassers/neodoollae/view/LoginActivity.kt
@@ -36,7 +36,9 @@ class LoginActivity : AppCompatActivity() {
 
     private val permissions = listOf(
         Manifest.permission.READ_EXTERNAL_STORAGE,
-        Manifest.permission.ACCESS_FINE_LOCATION
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
     )
 
     private val bind by lazy {
